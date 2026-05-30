88c06ec08568b4de62018734df51f04582d292ff
Fernando Yuji Távora Pacheco <305817@dac.unicamp.br>
2026-05-30 19:56:09 -0300

    player vira com base na posição do oponente

--- cena_jogador/player1.gd
+++ cena_jogador/player1.gd
@@ -1,56 +1,71 @@
-extends Sprite2D
-
+extends Node2D
 var ataquefraco = preload("res://cena_jogador/ataque_fraco.tscn")
 var ataqueforte = preload("res://cena_jogador/ataque_forte.tscn")
-var pos = [1500,540]
+var pos = [540,540]
+var bpm = 60/60
 var vida = 100
-var bpm = 1
+var orientacao = "direita"
+@export var player2 : Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.position = Vector2(pos[0], pos[1])
 	
 func ataque_fraco(player_x, player_y):
 	var fraco = ataquefraco.instantiate()
-	fraco.position = Vector2(player_x-180, player_y+60)
+	if orientacao == "direita":
+		fraco.position = Vector2(player_x+180, player_y+60)
+	else:
+		fraco.position = Vector2(player_x-180, player_y+60)
 	get_parent().add_child(fraco)
 	await get_tree().create_timer(bpm).timeout
 	fraco.queue_free()
 
 func ataque_forte(player_x, player_y):
 	var forte = ataqueforte.instantiate()
-	forte.position = Vector2(player_x-180, player_y-60)
+	if orientacao == "direita":
+		forte.position = Vector2(player_x+180, player_y-60)
+	else:
+		forte.position = Vector2(player_x-180, player_y-60)
 	get_parent().add_child(forte)
 	await get_tree().create_timer(bpm).timeout
 	forte.queue_free()
 var morreu = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	if self.position.x > player2.position.x:
+		orientacao = "esquerda"
+		$Sprite2D.flip_h = true
+	else:
+		orientacao = "direita"
+		$Sprite2D.flip_h = false
 	if morreu:
 		return
 	if vida <= 0:
 		print("morreu")
 		morreu = true
-	if Input.is_action_just_pressed("cima2"):
+	if Input.is_action_just_pressed("cima1"):
 		pos[1] -= 120
 		await get_tree().create_timer(bpm).timeout
 		pos[1] += 120
-	if Input.is_action_just_pressed("esquerda2"):
+	if Input.is_action_just_pressed("esquerda1"):
 		pos[0] -= 120
-	if Input.is_action_just_pressed("direita2"):
+	if Input.is_action_just_pressed("direita1"):
 		pos[0] += 120
-	if Input.is_action_just_pressed("fraco2"):
+	if Input.is_action_just_pressed("fraco1"):
 		ataque_fraco(pos[0], pos[1])
-	if Input.is_action_just_pressed("forte2"):
+	if Input.is_action_just_pressed("forte1"):
 		ataque_forte(pos[0], pos[1])
 	self.position = Vector2(pos[0], pos[1])
-	print(delta)
 
 
-func _on_hurtbox_2d_2_area_entered(area: Area2D) -> void:
+func _on_hurtbox_2d_1_area_entered(area: Area2D) -> void:
 	if area.get_name() == "fraco":
 		print("fraco")
 		vida -= 10
+		print("player 1 " + str(vida))
 	elif area.get_name() == "forte":
 		print("forte")
 		vida -= 20
-	print(vida)
+		print("player 1 " + str(vida))
