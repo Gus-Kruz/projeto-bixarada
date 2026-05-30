9e5a3b44cca92ff16797b7a8cd831a8ba19f4650
Fernando Yuji Távora Pacheco <305817@dac.unicamp.br>
2026-05-30 17:28:50 -0300

    sei la

--- cena_jogador/sprite_2d.gd
+++ cena_jogador/sprite_2d.gd
@@ -1,17 +1,49 @@
 extends Sprite2D
+
+var ataquefraco = preload("res://cena_jogador/ataque_fraco.tscn")
+var ataqueforte = preload("res://cena_jogador/ataque_forte.tscn")
 var pos = [1500,540]
 var vida = 100
+var bpm = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.position = Vector2(pos[0], pos[1])
+	
+func ataque_fraco(player_x, player_y):
+	var fraco = ataquefraco.instantiate()
+	fraco.position = Vector2(player_x-180, player_y+60)
+	get_parent().add_child(fraco)
+	await get_tree().create_timer(bpm).timeout
+	fraco.queue_free()
 
-
+func ataque_forte(player_x, player_y):
+	var forte = ataqueforte.instantiate()
+	forte.position = Vector2(player_x-180, player_y-60)
+	get_parent().add_child(forte)
+	await get_tree().create_timer(bpm).timeout
+	forte.queue_free()
+var morreu = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if morreu:
+		return
 	if vida <= 0:
 		print("morreu")
-	else:
-		print(vida)
+		morreu = true
+	if Input.is_action_just_pressed("cima2"):
+		pos[1] -= 120
+		await get_tree().create_timer(bpm).timeout
+		pos[1] += 120
+	if Input.is_action_just_pressed("esquerda2"):
+		pos[0] -= 120
+	if Input.is_action_just_pressed("direita2"):
+		pos[0] += 120
+	if Input.is_action_just_pressed("fraco2"):
+		ataque_fraco(pos[0], pos[1])
+	if Input.is_action_just_pressed("forte2"):
+		ataque_forte(pos[0], pos[1])
+	self.position = Vector2(pos[0], pos[1])
+	print(delta)
 
 
 func _on_hurtbox_2d_2_area_entered(area: Area2D) -> void:
@@ -21,3 +53,4 @@ func _on_hurtbox_2d_2_area_entered(area: Area2D) -> void:
 	elif area.get_name() == "forte":
 		print("forte")
 		vida -= 20
+	print(vida)
--- sprite_2d.gd
+++ sprite_2d.gd
@@ -2,7 +2,8 @@ extends Sprite2D
 var ataquefraco = preload("res://cena_jogador/ataque_fraco.tscn")
 var ataqueforte = preload("res://cena_jogador/ataque_forte.tscn")
 var pos = [540,540]
-var bpm = 1
+var bpm = 60/60
+var vida = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.position = Vector2(pos[0], pos[1])
@@ -20,9 +21,14 @@ func ataque_forte(player_x, player_y):
 	get_parent().add_child(forte)
 	await get_tree().create_timer(bpm).timeout
 	forte.queue_free()
-
+var morreu = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if morreu:
+		return
+	if vida <= 0:
+		print("morreu")
+		morreu = true
 	if Input.is_action_just_pressed("cima1"):
 		pos[1] -= 120
 		await get_tree().create_timer(bpm).timeout
@@ -36,3 +42,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("forte1"):
 		ataque_forte(pos[0], pos[1])
 	self.position = Vector2(pos[0], pos[1])
+
+
+func _on_hurtbox_2d_1_area_entered(area: Area2D) -> void:
+	if area.get_name() == "fraco":
+		print("fraco")
+		vida -= 10
+	elif area.get_name() == "forte":
+		print("forte")
+		vida -= 20
+	print(vida)
