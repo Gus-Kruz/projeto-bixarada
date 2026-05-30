db79294a693f2366e42b8c49fb3f6fab1b54429f
luqhe <90876462+luqhe@users.noreply.github.com>
2026-05-30 12:57:19 -0300

    toquezinho e erro na acumulação da média

--- scripts/calibracao.gd
+++ scripts/calibracao.gd
@@ -1,25 +1,56 @@
 extends Node
 
 var timerTimes: Array;
-var clickTimes: Array;
+var downTimes: Array;
+var diffTimes: Array;
 
 # Called when the node enters the scene tree for the first time.
 var timer;
+var audio;
 var sprite;
+var text;
 func _ready() -> void:
 	timer = $Timer;
+	audio = $AudioStreamPlayer;
 	sprite = $Sprite2D;
+	text = $TextEdit;
+	
+func update_text() -> void:
+	var sum = float(diffTimes.reduce(func(accum, num): return accum+num));
+	var mean = sum/len(diffTimes);
+	
+	var stdDev = (diffTimes.reduce(func(accum, num): return accum+(num-mean)**2))/len(diffTimes);
+	
+	text.set_line(0, 'Offset: {0}ms\nStdDev: {1}ms'.format([snapped(mean/1000, 0.01), snapped(stdDev/1000, 0.01)]));
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Flag para restringir atualizações de diffTimes
+var updateFlag: bool = false;
 func _process(delta: float) -> void:
-	pass;
+	if Input.is_action_just_pressed("cima2"): downTimes.append(Time.get_ticks_usec());
+	
+	if len(timerTimes) == len(downTimes) and updateFlag:
+		diffTimes.append(downTimes[-1]-timerTimes[-1]);
+		updateFlag = false;
+		
+		update_text();
+
+func show_block() -> void:
+	sprite.visible = true;
+	await get_tree().create_timer(0.2).timeout;
+	sprite.visible = false;
 
 func _on_pressed() -> void:
 	timer.start();
+	
+	# Espera pois tick do áudio acontece antes de 1s
+	await get_tree().create_timer(0.21).timeout;
+	audio.play();
 
 func _on_timer_timeout() -> void:
-	sprite.visible = true;
+	show_block();
 	timerTimes.append(Time.get_ticks_usec());
-	print(timerTimes);
-	await get_tree().create_timer(0.2).timeout;
-	sprite.visible = false;
+	updateFlag = true;
+
+# Gambiarra pois audio loop forward não funciona?
+func _on_audio_stream_player_finished() -> void:
+	audio.play()
