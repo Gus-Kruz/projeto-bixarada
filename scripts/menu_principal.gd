72a6a1a9180aff7084602b14a96649a873ff9591
Gustavo Silva da Cruz <g288603@dac.unicamp.br>
2026-05-31 09:02:04 -0300

    inicia o easter egg 67

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var threshold = 670
+var is_six: bool = true
+var easter = 0
 
 func _on_jogar_pressed() -> void:
 	pass
@@ -13,3 +16,15 @@ func _on_créditos_pressed() -> void:
 
 func _on_sair_pressed() -> void:
 	get_tree().quit()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed('six') and is_six == true:
+		is_six = false
+		easter += 67
+	if Input.is_action_just_pressed('seven') and is_six == false:
+		is_six = true
+		easter += 67
+	if easter >= threshold:
+		get_tree().quit()
+	easter -= 5
+	
