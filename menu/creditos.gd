47da1352fc8a479a61657f866a83b9e3a2dfaae9
Gustavo Silva da Cruz <g288603@dac.unicamp.br>
2026-05-31 02:36:01 -0300

    termina o template dos créditos

---
+++ menu/creditos.gd
@@ -0,0 +1,7 @@
+extends MarginContainer
+
+
+
+func _on_texture_button_pressed() -> void:
+	print("lÁ ELE PT 4")
+	get_tree().change_scene_to_file("res://menu/menu_principal.tscn")
--- menu/menu_principal.gd
+++ menu/menu_principal.gd
@@ -10,7 +10,7 @@ func _on_configurações_pressed() -> void:
 	# get_tree().change_scene_to_file("res://caminho_para_tela_configuração")
 
 func _on_créditos_pressed() -> void:
-	print("lá ele da silva")
+	pass
 	# get_tree().change_scene_to_file("res://caminho_para_tela_creditos")
 
 
