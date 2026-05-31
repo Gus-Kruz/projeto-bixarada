65ba51a269c9656d8d1db5f07f94fc6edcbaf8f5
eduardo <eduardodeoliveirascarcella@gmail.com>
2026-05-30 23:33:18 -0300

    adiciona mensagem de acerto

--- nota_amarela.gd
+++ nota_amarela.gd
@@ -1,15 +1,21 @@
 extends Area2D
-
+@export var mensagem_boa:Area2D
 var BPM = 80
 var cond = 0
 var dentro = false
-var yipe = false
+
+signal yipe
+func _ready() -> void:
+	var mensagem_boa = get_node("../mensagem_boa")
+	yipe.connect(mensagem_boa.on_yipe)
+
+
 
 func _physics_process(delta: float) -> void:
 	position.y += BPM*delta
 	if dentro:
 		if Input.is_action_just_pressed("cima1"):
-			yipe = true
+			yipe.emit()
 			position.y = 1
 			dentro = false
 
@@ -20,5 +26,4 @@ func _on_teclado_amarelo_area_entered(area: Area2D) -> void:
 func _on_teclado_amarelo_area_exited(area: Area2D) -> void:
 	dentro = false
 	position.y = 1
-	yipe = false
 	pass # Replace with function body.
